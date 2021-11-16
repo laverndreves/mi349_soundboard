4f52d36304c238179e101a045ca1d67fc681c71e
laverndreves <89997221+laverndreves@users.noreply.github.com>
2021-11-16 13:09:14 -0500

    asd

--- soundboard.js
+++ soundboard.js
@@ -1,2 +1,18 @@
 const sounds = ['gunfire', 'air raid', 'Cannon'];
 
+sounds.forEach(sound => {
+
+btn.addEventListener('click', () => {
+    stopPlaying();
+    document.getElementById(sound).Play(); 
+})
+document.getElementById('buttons').append(btn)
+
+})
+function stopPlaying(){
+    sounds.forEach(sound =>{
+        const audio = document.getElementById(sound);
+        audio.pause(); 
+        audio.currentTime = 0; 
+    })
+}
